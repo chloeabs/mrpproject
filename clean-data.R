2718ee3e60a6611c936c24e10c29fd2cb258eb76
Tim Dennis <dennis.tim@gmail.com>
2018-03-28 06:48:09 -0700

    household merge

--- clean-data.R
+++ clean-data.R
@@ -8,11 +8,31 @@ MRPdata_b <- read_xls('raw_data/EPH_usu_3_Trim_2017_xls/2017.3.copy.xls')
 MRPdata_merge <- bind_rows(MRPdata, MRPdata_b)
 
 mrpanalysis <- MRPdata_merge %>% 
-  select(CODUSU, REGION, MAS_500, AGLOMERADO, CH03, CH05:CH06,
-         CH10:CH12, CH16, NIVEL_ED, DECIFR, RDECIFR, GDECIFR,
-         PDECIFR, ADECIFR, DECCFR, RDECCFR, GDECCFR, PDECCFR, ADECCFR) %>% 
-  filter(CH06 >= 18 & CH06 <= 23)
+  select(CODUSU, REGION, CH03, CH05, NIVEL_ED) %>% 
+  rename(hh_id = CODUSU, region=REGION, family_relation = CH03, dob = CH05, edu_attain = NIVEL_ED) %>% 
+  tibble::rowid_to_column("id")
+
+head(mrpanalysis)
+
+#marp
+#create data set on dads == 1 
+dad_mrp <- mrpanalysis %>% filter(family_relation == 1) %>% 
+  mutate(dad_edu = edu_attain)
+
+#mom's data 
+mom_mrp <- mrpanalysis %>% 
+  filter(family_relation == 2) %>% 
+  mutate(mom_edu = edu_attain)
+
+
+mrp_fam_merge <- mrpanalysis %>%  left_join(dad_mrp) %>% left_join(mom_mrp) 
+
+head(mrp_fam_merge)
+
+group_by(hh_id) %>% fill(dad_edu)
+
 
 # need to grab NIVEL_ED from parents (CHO3 1 and 2 ) to children,
 # if CHO3 == 1 o 2 copy NIVEL_ED to children in household
-# what about the percentiles
+# what about the perc?entiles
+glimpse(mrpanalysis)
