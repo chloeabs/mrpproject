d7afc3e51462587d257d84532d89708ff7105a99
Tim Dennis <dennis.tim@gmail.com>
2018-03-29 08:13:43 -0700

    getting mom and dad education level into variables by household

--- clean-data.R
+++ clean-data.R
@@ -2,39 +2,41 @@
 library(readr)
 library(dplyr)
 library(readxl)
+
+# read data in
 MRPdata <- read_xls('raw_data/EPH_usu_2_Trim_2017_xls/2017.2.copy.xls')
 MRPdata_b <- read_xls('raw_data/EPH_usu_3_Trim_2017_xls/2017.3.copy.xls')
 
+# merge them together
 MRPdata_merge <- bind_rows(MRPdata, MRPdata_b)
 
+#select and rename the variables/columns you want to work with 
 mrpanalysis <- MRPdata_merge %>% 
   select(CODUSU, REGION, CH03, CH05, NIVEL_ED) %>% 
-  rename(hh_id = CODUSU, region=REGION, family_relation = CH03, dob = CH05, edu_attain = NIVEL_ED) %>% 
-  tibble::rowid_to_column("id")
+  rename(hh_id = CODUSU, region=REGION, family_relation = CH03, dob = CH05, edu_attain = NIVEL_ED) 
+  #tibble::rowid_to_column("id")
+
+#we can save the dataframe in an R native data format
 save(mrpanalysis, file="mrpanalysis.RData")
-head(mrpanalysis)
 
-#marp
-#create data set on dads == 1 
-dad_mrp <- mrpanalysis %>% filter(family_relation == 1) %>% 
-  mutate(dad_edu = edu_attain)
+#the below would load a dataframe you saved with save()
+#the idea here is you'll want to read, clean, prep the data, save() it, then you can load it when
+#you are ready for analysis 
+#load('mrpanalysis.RData')
 
-#mom's data 
-mom_mrp <- mrpanalysis %>% 
-  filter(family_relation == 2) %>% 
-  mutate(mom_edu = edu_attain)
+#takes a bit to run, but this sequence creates the mom and dad education vars based on family_relation
+#then groups the data by household and fills out those mom_edu & dad_edu vars by household
+# so each household will have a mom and dad education level with those values
+mrpan_mom_dad <- mrpanalysis %>% 
+  mutate(dad_edu = ifelse(family_relation == 1, edu_attain, NA),
+         mom_edu = ifelse(family_relation == 2, edu_attain, NA)) %>% 
+  group_by(hh_id) %>% 
+  fill(dad_edu, mom_edu) %>% 
+  fill(dad_edu, mom_edu, .direction = "up")
 
-
-mrp_fam_merge <- mrpanalysis %>%  left_join(dad_mrp) %>% left_join(mom_mrp) 
-
-save(mrp_fam_merge, file="mrp_fam_merge.Rdata")
-
-head(mrp_fam_merge)
-
-group_by(hh_id) %>% fill(dad_edu)
+save(mrpan_mom_dad, file="mrpan_mom_dad.Rdata")
 
 
 # need to grab NIVEL_ED from parents (CHO3 1 and 2 ) to children,
 # if CHO3 == 1 o 2 copy NIVEL_ED to children in household
 # what about the perc?entiles
-glimpse(mrpanalysis)
